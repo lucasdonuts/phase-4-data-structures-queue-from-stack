fc0dbc513b44a1dda3028b4ae465fa883c393033
Lucas Duncan <lucasdonuts@gmail.com>
2022-09-01 14:59:15 -0500

    All tests passing

--- lib/queue_from_stack.rb
+++ lib/queue_from_stack.rb
@@ -1,3 +1,29 @@
 require_relative './stack'
 
-# your code here
+class MyQueue
+  attr_reader :s1, :s2
+
+  def initialize
+    @s1 = Stack.new
+    @s2 = Stack.new
+  end
+
+  def add(value)
+    if s1.empty?
+      s1.push(value)
+    else
+      s2.push(s1.pop) until s1.empty?
+      s1.push(value)
+      s1.push(s2.pop) until s2.empty?
+    end
+  end
+
+  def remove
+    s1.pop
+  end
+
+  def peek
+    s1.peek
+  end
+
+end
